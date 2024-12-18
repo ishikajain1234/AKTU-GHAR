9a79ae1745e4d22a6a33c7192ee6aaf4e059e95f
jainishika072@gmail.com <jainishika072@gmail.com>
2024-12-18 12:24:18 +0530

    done

--- LMS-website/server/models/course.model.js
+++ LMS-website/server/models/course.model.js
@@ -1,54 +1,57 @@
 import mongoose from "mongoose";
 
-const courseSchema = new mongoose.Schema({
-  courseTitle: {
-    type: String,
-    required: true,
-  },
-  subTitle: {
-    type: String,
-    // required:true
-  },
-  description: {
-    type: String,
-    // required:true
-  },
-  price: {
-    type: Number,
-    // required:true
-  },
-  courseThumbnail: {
-    type: String,
-    // required:true
-  },
-  category: {
-    typwe: String,
-    required: true,
-  },
-  courseLevel: {
-    type: String,
-    enum: ["Beginner", "Intermediate", "Advanced"],
-  },
-  enrolledStudents: [
-    {
+const courseSchema = new mongoose.Schema(
+  {
+    courseTitle: {
+      type: String,
+      required: true,
+    },
+    subTitle: {
+      type: String,
+      // required: true
+    },
+    description: {
+      type: String,
+      // required: true
+    },
+    price: {
+      type: Number,
+      // required: true
+    },
+    courseThumbnail: {
+      type: String,
+      // required: true
+    },
+    category: {
+      type: String, // Fixed the typo here
+      required: true,
+    },
+    courseLevel: {
+      type: String,
+      enum: ["Beginner", "Intermediate", "Advanced"],
+    },
+    enrolledStudents: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lectures: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lecture",
+      },
+    ],
+    creator: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-  ],
-  lectures: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Lecture",
+    isPublished: {
+      type: Boolean,
+      default: false,
     },
-  ],
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
   },
-  isPublished: {
-    type: Boolean,
-    default: false,
-  }
-}, {timestamps:true});
+  { timestamps: true }
+);
 
 export default mongoose.model("Course", courseSchema);
